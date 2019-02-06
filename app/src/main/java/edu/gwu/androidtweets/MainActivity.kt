56820d401f829369fe8f88f3a56afcbe83f835bd
Nick Capurso <capurso.nick@gmail.com>
2019-02-05 21:49:46 -0500

    Lecture 3 - Finish Basic Login and Intents

--- app/src/main/java/edu/gwu/androidtweets/MainActivity.kt
+++ app/src/main/java/edu/gwu/androidtweets/MainActivity.kt
@@ -1,12 +1,87 @@
 package edu.gwu.androidtweets
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var username: EditText
+
+    private lateinit var password: EditText
+
+    private lateinit var login: Button
+
+    private lateinit var progressBar: ProgressBar
+
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val inputtedUsername: String = username.text.toString().trim()
+            val inputtedPassword: String = password.text.toString().trim()
+            val enableButton: Boolean = inputtedUsername.isNotEmpty() && inputtedPassword.isNotEmpty()
+
+            login.isEnabled = enableButton
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "onCreate called")
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+
+        username.addTextChangedListener(textWatcher)
+        password.addTextChangedListener(textWatcher)
+
+        login.setOnClickListener {
+            Log.d("MainActivity", "Login Clicked")
+            progressBar.visibility = View.VISIBLE
+
+            val intent: Intent = Intent(this, TweetsActivity::class.java)
+            intent.putExtra("location", "Washington D.C.")
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy called")
     }
 }
