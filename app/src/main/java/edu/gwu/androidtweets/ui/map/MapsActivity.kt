eef7afc289d49416700c7ce589aab0806ef2e04e
Nick Capurso <nickcapurso@nickcapurso-mbp.fios-router.home>
2019-03-25 18:27:58 -0400

    MapsActivity - Cleanup comment

--- app/src/main/java/edu/gwu/androidtweets/ui/map/MapsActivity.kt
+++ app/src/main/java/edu/gwu/androidtweets/ui/map/MapsActivity.kt
@@ -142,13 +142,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        // val sydney = LatLng(-34.0, 151.0)
-        // mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-
         mMap.setOnMapLongClickListener { coordinates ->
             // Code in here executes whenever the user long-presses
             googleMap.clear()
